3d96ff9a9860c0df1ce945fa80a174dbd0310dd7
erikwehrmann <erik.wehrmann@gmail.com>
2022-11-22 23:48:13 +0000

    Timer 2 function

---
+++ timer2.js
@@ -0,0 +1,19 @@
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+stdin.on('data', (key) => {
+  if (key == '\u0003') {
+    console.log("Thanks for using me, ciao!");
+    process.exit();
+  }
+  if (key == 'b') {
+    process.stdout.write('\x07');
+  }
+  if (key > 0) {
+    console.log(`setting timer for ${key} seconds...`);
+    setTimeout (() => {
+      process.stdout.write('\x07');
+    }, (key * 1000));
+  }
+})
